f216a4b8492fe29dd223b318996db1f212471214
Nan Ma <nanma@microsoft.com>
2022-04-06 08:51:45 -0700

    435 534 1000

--- all_honeycombs.ps1
+++ all_honeycombs.ps1
@@ -1,4 +1,5 @@
-wolframscript.exe .\honeycombs.wls 4 3 5
+# generate honeycomb cells, faces, edges, vertices
 wolframscript.exe .\honeycombs.wls 5 3 4
-wolframscript.exe .\honeycombs.wls 3 5 3
-wolframscript.exe .\honeycombs.wls 5 3 5
+wolframscript.exe .\honeycombs.wls 4 3 5
+# wolframscript.exe .\honeycombs.wls 3 5 3
+# wolframscript.exe .\honeycombs.wls 5 3 5
