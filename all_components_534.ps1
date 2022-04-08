2a8c17c722e1eae8590c5df81e976d0d998533a3
Nan Ma <nanma@microsoft.com>
2022-04-08 15:41:30 -0700

    viz 534

---
+++ all_components_534.ps1
@@ -0,0 +1,6 @@
+wolframscript.exe .\compound_534.wls 1
+wolframscript.exe .\compound_534.wls 2
+wolframscript.exe .\compound_534.wls 3
+wolframscript.exe .\compound_534.wls 4
+wolframscript.exe .\compound_534.wls 5
+wolframscript.exe .\compound_534.wls 0
