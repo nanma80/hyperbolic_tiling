09f515a529eb43fb6ec37b17dfa72c60a770d314
Nan Ma <nanma@microsoft.com>
2022-04-11 00:39:15 -0700

    cell first

--- all_components_534.ps1
+++ all_components_534.ps1
@@ -1,6 +1,6 @@
-wolframscript.exe .\compound_534.wls 1
-wolframscript.exe .\compound_534.wls 2
-wolframscript.exe .\compound_534.wls 3
-wolframscript.exe .\compound_534.wls 4
-wolframscript.exe .\compound_534.wls 5
-wolframscript.exe .\compound_534.wls 0
+wolframscript.exe .\compound_534_cell_first.wls 1
+wolframscript.exe .\compound_534_cell_first.wls 2
+wolframscript.exe .\compound_534_cell_first.wls 3
+wolframscript.exe .\compound_534_cell_first.wls 4
+wolframscript.exe .\compound_534_cell_first.wls 5
+wolframscript.exe .\compound_534_cell_first.wls 0
