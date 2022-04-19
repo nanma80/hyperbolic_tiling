c7fafb944dd586e817ce2ca2a659ecf83caf1367
Nan Ma <nanma@microsoft.com>
2022-04-19 10:28:40 -0700

    rendered components 1,2

--- all_explosion.ps1
+++ all_explosion.ps1
@@ -1,7 +1,7 @@
-wolframscript.exe .\explode_compound_534.wls 1 1 False
-wolframscript.exe .\explode_compound_534.wls 1 2 False
-wolframscript.exe .\explode_compound_534.wls 2 1 False
-wolframscript.exe .\explode_compound_534.wls 2 2 False
+#wolframscript.exe .\explode_compound_534.wls 1 1 False
+#wolframscript.exe .\explode_compound_534.wls 1 2 False
+#wolframscript.exe .\explode_compound_534.wls 2 1 False
+#wolframscript.exe .\explode_compound_534.wls 2 2 False
 wolframscript.exe .\explode_compound_534.wls 0 1 False
 wolframscript.exe .\explode_compound_534.wls 0 2 False
 wolframscript.exe .\explode_compound_534.wls 0 1 True
