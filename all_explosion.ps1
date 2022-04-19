1075d8c27c706b0eb297afbb1e9393e9b64b4f89
Nan Ma <nanma@microsoft.com>
2022-04-19 00:39:57 -0700

    adjust opacity logic

--- all_explosion.ps1
+++ all_explosion.ps1
@@ -1,17 +1,13 @@
-# wolframscript.exe .\explode_compound_534.wls 0 2 False
-# wolframscript.exe .\explode_compound_534.wls 0 1 False
-# wolframscript.exe .\explode_compound_534.wls 0 2 True
-# wolframscript.exe .\explode_compound_534.wls 1 2 False
 wolframscript.exe .\explode_compound_534.wls 1 1 False
-wolframscript.exe .\explode_compound_534.wls 2 2 False
+wolframscript.exe .\explode_compound_534.wls 1 2 False
 wolframscript.exe .\explode_compound_534.wls 2 1 False
-wolframscript.exe .\explode_compound_534.wls 3 2 False
-wolframscript.exe .\explode_compound_534.wls 3 1 False
-wolframscript.exe .\explode_compound_534.wls 4 2 False
-wolframscript.exe .\explode_compound_534.wls 4 1 False
-wolframscript.exe .\explode_compound_534.wls 5 2 False
-wolframscript.exe .\explode_compound_534.wls 5 1 False
-wolframscript.exe .\explode_compound_534.wls 0 2 False
+wolframscript.exe .\explode_compound_534.wls 2 2 False
 wolframscript.exe .\explode_compound_534.wls 0 1 False
+wolframscript.exe .\explode_compound_534.wls 0 2 False
 wolframscript.exe .\explode_compound_534.wls 0 1 True
-wolframscript.exe .\explode_compound_534.wls 0 2 True
+wolframscript.exe .\explode_compound_534.wls 3 1 False
+wolframscript.exe .\explode_compound_534.wls 3 2 False
+wolframscript.exe .\explode_compound_534.wls 4 1 False
+wolframscript.exe .\explode_compound_534.wls 4 2 False
+wolframscript.exe .\explode_compound_534.wls 5 1 False
+wolframscript.exe .\explode_compound_534.wls 5 2 False
