f9cc37efae4890620c36cb968c313b06a31783e0
Nan Ma <nanma@microsoft.com>
2022-04-20 11:00:33 -0700

    new videos

--- all_explosion.ps1
+++ all_explosion.ps1
@@ -2,7 +2,7 @@
 #wolframscript.exe .\explode_compound_534.wls 1 2 False
 #wolframscript.exe .\explode_compound_534.wls 2 1 False
 #wolframscript.exe .\explode_compound_534.wls 2 2 False
-wolframscript.exe .\explode_compound_534.wls 0 1 False
+#wolframscript.exe .\explode_compound_534.wls 0 1 False
 wolframscript.exe .\explode_compound_534.wls 0 2 False
 wolframscript.exe .\explode_compound_534.wls 0 1 True
 wolframscript.exe .\explode_compound_534.wls 3 1 False
